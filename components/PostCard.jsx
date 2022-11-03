0b9e182dd416de872e9996a3bbc7561e11c8c038
rudy0628 <s3352250zz@gmail.com>
2022-11-03 11:45:51 +0800

    PostCard finished

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -3,8 +3,6 @@ import moment from 'moment/moment';
 import Link from 'next/link';
 
 const PostCard = ({ post }) => {
-	console.log(post);
-
 	return (
 		<div className="bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8">
 			<div className="relative overflow-hidden shadow-md pb-80 mb-6">
