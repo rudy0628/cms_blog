01bfa67f030a2d755e45df7709f8c3a829c9c0da
rudy0628 <s3352250zz@gmail.com>
2022-11-03 11:45:22 +0800

    Remove the dummy categories and get categories from CMS

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,13 +1,18 @@
-import React, { useContext } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Link from 'next/link';
-
-const categories = [
-	{ name: 'React', slug: 'react' },
-	{ name: 'Web Development', slug: 'web-dev' },
-];
+import { shuffleArray } from '../utils';
+import { getCategories } from '../services';
 
 const Header = () => {
+	const [categories, setCategories] = useState([]);
+
+	useEffect(() => {
+		getCategories().then(result =>
+			setCategories(shuffleArray(result).slice(0, 5))
+		);
+	}, []);
+
 	return (
 		<div className="container mx-auto px-10 mb-8">
 			<div className="border-b w-full inline-block border-blue-400 py-8">
